97ef802156cf6139300787481dfbf43a903ad464
Arash <aelahi4@uic.edu>
2025-02-11 14:43:39 -0600

    Removed unwanted files

--- scripts/main.py
+++ scripts/main.py
@@ -1,18 +1,16 @@
 from CNCstruc.structure import CNC_class as CNC
-# from CNCstruc.structure import CNC_class as CNC
 from CNCstruc.utils import traj_reader as trj
-from CNCstruc.analysis import Indexing.py as indx_gen
+from CNCstruc.analysis import Indexing as indx_gen
+
 import os 
 
+## initialization of the CNC object
 CNC_form = 'Pristine'
 filepath= ''
-# filepath = '/Users/arashelahi/Research/Papers/CNC_model/Review/Charmm/'
-# filename = filepath + 'reordered_solute.gro'
 filename = f'./data/input/{CNC_form}/solute.gro'
-
 Data = trj.gro_reader(filename)
-CNC_group = CNC.CNC_analys(Data )
+CNC_group = CNC.CNC_analys(Data)
+## indexing a target feature
 feature = 'H_bonds'
 CNC_group.get_indices(feature=feature)
-# print('hi')
 indx_gen.ndx_making(CNC_group, feature , output_path=filepath)
